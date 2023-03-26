09dc115231035905cc3400739f0fafedf71218a8
tcvduc <caovanducs@gmail.com>
2023-03-27 01:37:33 +0700

    onboard map

--- 2.map/1.js
+++ 2.map/1.js
@@ -14,4 +14,37 @@ x.set(3, 4);
 x.set(5, 2);
 x.set(6, 2);
 
-console.log(x);
+function f1() {
+  console.log(x);
+
+  console.log(x.get(1));
+}
+
+function f2() {
+  const a = [1, 2, 3, 4, 5];
+  const map = new Map();
+
+  for (let i = 0; i <= a.length - 1; ++i) {
+    map.set(i, a[i]);
+  }
+
+  console.log(map);
+}
+
+function f3() {
+  const a = [1, 2, 3, 4, 5];
+  const map = new Map();
+
+  for (let i = 0; i <= a.length - 1; ++i) {
+    map.set(a[i], i);
+  }
+
+  console.log(map);
+}
+
+{
+  //   f1();
+  //   f2();
+
+  f3();
+}
