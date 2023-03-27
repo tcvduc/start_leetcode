7019dce556f033a276ef79ba1f6ca0aa484a9a68
tcvduc <caovanducs@gmail.com>
2023-03-27 13:57:14 +0700

    onboard algorithm

---
+++ again/3/1.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  /**
+   * ---------0 1 2 3 4
+   * + nums: [1,2,3,4,5]
+   * + target: 9
+   * + ret: [3,4]
+   *
+   * Equation
+   * x = target - nums[i]
+   *
+   */
+  const map = new Map();
+
+  for (let i = 0; i <= nums.length - 1; ++i) {
+    const x = target - nums[i];
+
+    if (map.has(x)) {
+      return [map.get(x), i];
+    }
+
+    map.set(nums[i], i);
+  }
+};
+
+const nums = [1, 2, 3, 4, 5];
+const target = 9;
+const ret = twoSum(nums, target);
+console.log(ret);
