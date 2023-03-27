20004345595936bc248eb1db0f7bfa62bb43d13d
tcvduc <caovanducs@gmail.com>
2023-03-27 14:56:49 +0700

    onboard algorithm

--- again/2/2.js
+++ again/2/2.js
@@ -43,15 +43,31 @@ var addTwoNumbers = function (l1, l2) {
   l2.show();
 
   const result = new ListNode(undefined, undefined);
+  let wasReminder = false;
 
-  while (l1.next !== null || l2.next !== null) {
-    const vl1 = l1.val;
-    const vl2 = l2.val;
+  while (true) {
+    if (l1 !== null) {
+      if (l2 === null) {
+        if (wasReminder === true) {
+        }
 
-    console.log(vl1);
+        if (wasReminder === false) {
+        }
+      }
 
-    l1 = l1.next;
-    l2 = l2.next;
+      if (l2 !== null) {
+      }
+
+      const vl1 = l1.val;
+      const vl2 = l2.val;
+      l1 = l1.next;
+      l2 = l2.next;
+    }
+
+    if (l1 === null) {
+      console.log(l1);
+      break;
+    }
   }
 };
 
@@ -60,7 +76,8 @@ function test1() {
   const n2 = new ListNode(2, n3);
   const l1 = new ListNode(1, n2);
 
-  const n6 = new ListNode(6, null);
+  const n7 = new ListNode(7, null);
+  const n6 = new ListNode(6, n7);
   const n5 = new ListNode(5, n6);
   const l2 = new ListNode(4, n5);
 
