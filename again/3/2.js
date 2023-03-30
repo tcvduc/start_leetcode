29e438052811fd54867aabae82899c9f649242af
tcvduc <caovanducs@gmail.com>
2023-03-30 13:34:47 +0700

    onboard algorithm

--- again/3/2.js
+++ again/3/2.js
@@ -435,6 +435,21 @@ function test7() {
   result.show();
 }
 
+function test8() {
+  const n14 = new ListNode(4, null);
+  const n13 = new ListNode(3, n14);
+  const n12 = new ListNode(9, n13);
+  const l1 = new ListNode(9, n12);
+
+  const n24 = new ListNode(4, null);
+  const n23 = new ListNode(3, n24);
+  const n22 = new ListNode(9, n23);
+  const l2 = new ListNode(9, n22);
+
+  const result = addTwoNumbers(l1, l2);
+  result.show();
+}
+
 {
   // test1();
   // test2();
@@ -442,5 +457,6 @@ function test7() {
   // test4();
   // test5();
   // test6();
-  test7();
+  // test7();
+  test8();
 }
