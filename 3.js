f09fc055b9d5b36ce9b5242b4fc641650b237c97
tcvduc <caovanducs@gmail.com>
2023-03-31 19:09:54 +0700

    onboard english

---
+++ 3.js
@@ -0,0 +1,19 @@
+/**
+ * 3. Longest Substring Without Repeating Characters
+ * - Given a string s, find the length of the
+ * longest substring without repeating characters
+ *
+ * - Example 1
+ * + s = "abcabcbb"
+ * + output = 3
+ * + Explanation: The answer is "abc",
+ * with the length of 3.
+ *
+ * - Example 2
+ * + s = "bbbbb"
+ * + output = 1
+ * + Explanation: The answer is "b",
+ * with the length of 1.
+ *
+ *
+ */
