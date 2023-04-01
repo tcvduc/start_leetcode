a4e75d20d5f08f67134e7a3a2850073fdf5d60e5
tcvduc <caovanducs@gmail.com>
2023-04-01 19:16:07 +0700

    onboard algorithm

--- again/4/4.js
+++ again/4/4.js
@@ -3,13 +3,6 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays = function (nums1, nums2) {};
-
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number}
- */
 var findMedianSortedArrays = function (nums1, nums2) {
   const merge = [...nums1, ...nums2];
   merge.sort((a, b) => a - b);
--- again/5/4.js
+++ again/5/4.js
@@ -1,3 +1,26 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function (nums1, nums2) {
+  const merge = [...nums1, ...nums2];
+  merge.sort((a, b) => a - b);
+  const index = merge.length - 1;
+
+  if (index % 2 === 0) {
+    return merge[index / 2].toFixed(5);
+  }
+
+  if (index % 2 !== 0) {
+    const delta = (index + 1) / 2;
+    const v1 = merge[delta];
+    const v2 = merge[delta - 1];
+    const result = (v1 + v2) / 2;
+    return result.toFixed(5);
+  }
+};
+
 function test1() {
   const nums1 = [1, 3];
   const nums2 = [2];
