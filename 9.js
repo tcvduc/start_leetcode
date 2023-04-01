f1fa6c6d219d2422874b163f6beb99d220d36f53
tcvduc <caovanducs@gmail.com>
2023-04-02 02:26:09 +0700

    onboard algorithm

---
+++ 9.js
@@ -0,0 +1,20 @@
+/**
+ * 9. Palindrome Number
+ * - Given an integer "x", return true
+ * if x a palindrome number and false
+ * otherwise.
+ *
+ * - Example 1
+ * + x = 121
+ * + result = true
+ * + Explanation: 121 reads as 121 from left
+ * to right and from right to left.
+ *
+ * - Example 2
+ * + x = -121
+ * + result = false
+ * + Explanation: From left to right it reads
+ * -121, from right to left it becomes 121-.
+ * Therefore it is not a palindrome.
+ *
+ */
