9607561aaade3c691c5513ac881db97da7f7e995
tcvduc <caovanducs@gmail.com>
2023-04-01 16:53:45 +0700

    onboard algorithm

---
+++ again/3/5.js
@@ -0,0 +1,94 @@
+/**
+ *
+ * @param {String} s
+ */
+function wasPalindrome(s) {
+  let flag = true;
+  let index = s.length - 1;
+  const delta1 = s.length / 2;
+  const delta2 = (s.length + 1) / 2;
+
+  if (index % 2 === 0) {
+    for (let i = 0; i <= delta1; ++i) {
+      const j = s.length - i - 1;
+      if (s[i] !== s[j]) {
+        flag = false;
+        break;
+      }
+    }
+  }
+
+  if (index % 2 !== 0) {
+    for (let i = 0; i <= delta2; i++) {
+      const j = s.length - i - 1;
+      if (s[i] !== s[j]) {
+        flag = false;
+        break;
+      }
+    }
+  }
+
+  return flag;
+}
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  let max = 0;
+  let result = s[0];
+
+  for (let i = 0; i <= s.length - 1 - 1; ++i) {
+    let subString = s[i];
+    for (let j = i + 1; j <= s.length - 1; ++j) {
+      subString += s[j];
+      if (wasPalindrome(subString)) {
+        if (subString.length > max) {
+          max = subString.length;
+          result = subString;
+        }
+      }
+    }
+  }
+
+  return result;
+};
+
+function test1() {
+  const s = "babad";
+  const result = longestPalindrome(s);
+  console.log(result);
+}
+
+function test2() {
+  const s = "cbbd";
+  const result = longestPalindrome(s);
+  console.log(result);
+}
+
+function test3() {
+  const s = "a";
+  const result = longestPalindrome(s);
+  console.log(result);
+}
+
+function test4() {
+  const s = "ac";
+  const result = longestPalindrome(s);
+  console.log(result);
+}
+
+function test5() {
+  const s = "abcda";
+  const result = longestPalindrome(s);
+  console.log(result);
+}
+
+{
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
+}
