09049d6b21844797058d7ab1eff81a51bf88f29a
tcvduc <caovanducs@gmail.com>
2023-04-03 13:01:40 +0700

    onboard algorithm

--- 3.number/4.js
+++ 3.number/4.js
@@ -1,13 +1,18 @@
 const n = 1234;
+const n1 = n / 10;
+const n2 = n / 100;
+const n3 = (n / 1000).toFixed(0);
 
-console.log("");
+console.log(n1);
+console.log(n2);
+console.log(n3);
 
 const v1 = n % 10;
 const v2 = n % 100;
 const v3 = n % 1000;
 const v4 = n % 10000;
 
-console.log(v1);
-console.log(v2);
-console.log(v3);
-console.log(v4);
+// console.log(v1);
+// console.log(v2);
+// console.log(v3);
+// console.log(v4);
