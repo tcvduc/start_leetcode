7f91632dfd9f3a369686aa6d177eabd88b5f3529
tcvduc <caovanducs@gmail.com>
2023-04-16 19:25:20 +0700

    onboard algorithm

--- 14.js
+++ 14.js
@@ -18,47 +18,103 @@
  */
 
 /**
+ *
+ * @param {String[]} strs
+ *
+ */
+function getLongestStringLength(strs) {
+  let max = 0;
+
+  for (let i = strs.length - 1; i >= 0; --i) {
+    if (strs[i].length > max) {
+      max = strs[i].length;
+    }
+  }
+
+  return max;
+}
+
+/**
+ *
+ * @param {String[]} strs
+ *
+ */
+function isEmpty(strs) {
+  let flag = true;
+
+  for (let i = strs.length - 1; i >= 0; --i) {
+    if (strs[i] !== "") {
+      flag = false;
+      break;
+    }
+  }
+
+  return flag;
+}
+
+/**
  * @param {string[]} strs
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
-  const prefixs = [];
+  if (strs.length === 1) {
+    return strs[0];
+  }
 
+  const isEmptyFlag = isEmpty(strs);
+
+  if (isEmptyFlag) return "";
+
+  const max = getLongestStringLength(strs);
   const s0 = strs[0];
-  let preFlag = false;
   let index = 0;
+  let result = "";
 
-  for (let i = 1; i <= strs.length - 1; ++i) {
-    const si = strs[i];
+  for (let q = 0; q <= max; ++q) {
+    let flag = true;
+    let prefix = "";
+    for (let i = 1; i <= strs.length - 1; ++i) {
+      const si = strs[i];
 
-    if (si[index] !== s0[index]) {
-      preFlag = true;
-      break;
+      if (si[index] !== s0[index]) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag === false) {
+      index--;
+      prefix = "";
+
+      for (let k = 0; k <= index; ++k) {
+        prefix += s0[k];
+      }
+
+      return prefix;
+    }
+
+    if (flag === true) {
+      index++;
+
+      if (s0[index] === undefined) {
+        index--;
+
+        for (let k = 0; k <= index; ++k) {
+          prefix += s0[k];
+        }
+
+        result = prefix;
+        break;
+      }
+
+      for (let k = 0; k <= index; ++k) {
+        prefix += s0[k];
+      }
+
+      result = prefix;
     }
   }
 
-  if (preFlag === true) {
-    return "";
-  }
-
-  index++;
-  let secondFlag = false;
-
-  for (let i = 1; i <= strs.length - 1; ++i) {
-    const si = strs[i];
-
-    if (s0[index] !== si[index]) {
-      secondFlag = true;
-      break;
-    }
-  }
-
-  let longestPrefix = "";
-  for (let i = 0; i <= index; ++i) {
-    longestPrefix += s0[i];
-  }
-
-  return longestPrefix;
+  return result;
 };
 
 function test1() {
@@ -201,11 +257,25 @@ function test6() {
   console.log(result); // ""
 }
 
+function test7() {
+  const strs = ["", ""];
+  const result = longestCommonPrefix(strs);
+  console.log(result); // ""
+}
+
+function test8() {
+  const strs = ["flower", "flower", "flower", "flower"];
+  const result = longestCommonPrefix(strs);
+  console.log(result); // "flower"
+}
+
 {
   //   test1();
   //   test2();
-  test3();
+  //   test3();
   //   test4();
   //   test5();
   //   test6();
+  //   test7();
+  test8();
 }
