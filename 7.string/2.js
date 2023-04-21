54e3e50faae955965a0626d0045404a7859f60de
tcvduc <caovanducs@gmail.com>
2023-04-21 13:21:40 +0700

    onboard algorithm

--- 7.string/2.js
+++ 7.string/2.js
@@ -20,9 +20,46 @@
 
 /**
  *
+ * @param {Number} min
+ * @param {Number} max
+ *
+ */
+function generateRandomNumber(min, max) {
+  /**
+   * + min: 5
+   * + max: 10
+   * + result: [5,10]
+   *
+   * + math.floor
+   * + math.random: [0,0.9]
+   * + min: 5
+   * + max: 10
+   * + result: [5,10]
+   *
+   * + floor
+   *   + [5,10.e]
+   *   + [5,10]
+   *
+   *  + floor(delta x (max - min + 1) + min)
+   *  + [0,0.9] x (10 - 5 + 1) + 5
+   *    + [0,0.9] x 6 + 5
+   *    + [0,5.e] + 5
+   *    + [5,10.e]
+   *    + [5,10]
+   *
+   */
+  const delta = Math.random();
+  const result = Math.floor(delta * (max - min + 1) + min);
+  return result;
+}
+
+/**
+ *
  * @param {Number} n
  */
 function generateRandomString(n) {
+  if (n === 0) return "";
+
   let result = "";
 
   const characters =
@@ -41,8 +78,23 @@ function generateRandomString(n) {
 function test1() {
   const n = 3;
   const result = generateRandomString(n);
+  console.log(result);
+}
+
+function test2() {
+  const n = 0;
+  const result = generateRandomString(n);
+  console.log(result);
+}
+
+function test3() {
+  const n = 10;
+  const result = generateRandomString(n);
+  console.log(result);
 }
 
 {
   test1();
+  test2();
+  test3();
 }
