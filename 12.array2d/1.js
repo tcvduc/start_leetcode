6dfc64463e6d69dc9d7e17cb53ab4d65d16b8f24
tcvduc <caovanducs@gmail.com>
2023-04-27 17:50:14 +0700

    onboard array 2d

---
+++ 12.array2d/1.js
@@ -0,0 +1,38 @@
+/**
+ * 1. Init Array 2d
+ * - Given an array 2d concepts, let's init it and log
+ * into the console.
+ *
+ * - Example:
+ * + a = [[1,2],[3,4]]
+ * + result
+ * [[1,2],[3,4]]
+ *
+ */
+
+/**
+ *
+ * @param {Array[]} a
+ */
+function advanceLogArray2d(a) {
+  /**
+   * ------------0         1
+   * - a = [ [ 1, 2 ], [ 3, 4 ] ]
+   *
+   */
+}
+
+function onboardArray2d() {
+  const a = [
+    [1, 2],
+    [3, 4],
+  ];
+
+  const s1 = console.log(a);
+
+  console.log(s1);
+}
+
+{
+  onboardArray2d();
+}
