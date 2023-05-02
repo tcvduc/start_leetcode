bdab7f8bf04c4b1b53506a5ee0a770b722801421
tcvduc <caovanducs@gmail.com>
2023-05-02 16:31:11 +0700

    onboard algorithm

--- 20.js
+++ 20.js
@@ -30,26 +30,128 @@
  */
 
 /**
+ *
+ * @param {String} s1
+ * @param {String} s2
+ *
+ */
+function isTwoStringTheSame(s1, s2) {
+  if (s1.length !== s2.length) return false;
+  let flag = true;
+  for (let i = s1.length - 1; i >= 0; --i) {
+    if (s1[i] !== s2[i]) {
+      flag = false;
+      break;
+    }
+  }
+  return flag;
+}
+
+/**
+ *
+ * @param {String} o
+ * @param {String} c
+ *
+ */
+function isOpenAndCloseType1(o, c) {
+  /**
+   * + Open and close type 1
+   * + (: open1
+   * + ): close1
+   *
+   */
+  const open1s = "open1";
+  const close1s = "close1";
+  const flag1 = isTwoStringTheSame(open1s, o);
+  const flag2 = isTwoStringTheSame(close1s, c);
+  const result = flag1 && flag2;
+  return result;
+}
+
+/**
+ *
+ * @param {String} o
+ * @param {String} c
+ *
+ */
+function isOpenAndCloseType2(o, c) {
+  /**
+   * + Open and close type 2
+   * + [: open2
+   * + ]: close2
+   *
+   */
+  const open2s = "open2";
+  const close2s = "close2";
+  const flag1 = isTwoStringTheSame(open2s, o);
+  const flag2 = isTwoStringTheSame(close2s, c);
+  const result = flag1 && flag2;
+  return result;
+}
+
+/**
+ *
+ * @param {String} o
+ * @param {String} c
+ *
+ */
+function isOpenAndCloseType3(o, c) {
+  /**
+   * + Open and close type 3
+   * + [: open3
+   * + ]: close3
+   *
+   */
+  const open3s = "open3";
+  const close3s = "close3";
+  const flag1 = isTwoStringTheSame(open3s, o);
+  const flag2 = isTwoStringTheSame(close3s, c);
+  const result = flag1 && flag2;
+  return result;
+}
+
+/**
+ *
+ * @param {String} si
+ * @param {String} sip1
+ *
+ */
+function isOpenAndCloseTheSameTypeOfBracket(si, sip1) {
+  const map = new Map();
+  map.set("(", "open1");
+  map.set("[", "open2");
+  map.set("{", "open3");
+  map.set(")", "close1");
+  map.set("]", "close2");
+  map.set("}", "close3");
+
+  const openType = map.get(si);
+  const closeType = map.get(sip1);
+
+  const flag1 = isOpenAndCloseType1(openType, closeType);
+  const flag2 = isOpenAndCloseType2(openType, closeType);
+  const flag3 = isOpenAndCloseType3(openType, closeType);
+
+  return flag1 || flag2 || flag3;
+}
+
+/**
  * @param {string} s
  * @return {boolean}
  */
 const isValid = function (s) {
-  console.log(s);
   let flag = true;
   const length = s.length;
 
   if (length % 2 === 0) {
-    for (let i = 0; i <= s.length - 1; ) {
+    for (let i = 0; i <= s.length - 1; i += 2) {
       const si = s[i];
       const sip1 = s[i + 1];
-      console.log(si);
-      console.log(sip1);
-      if (si !== sip1) {
-        flag = false;
+      const flag1 = isOpenAndCloseTheSameTypeOfBracket(si, sip1);
+      if (!flag1) {
+        flag = flag1;
         break;
       }
-
-      i += 2;
     }
   }
 
@@ -98,9 +200,9 @@ function test6() {
 
 {
   test1(); // true
-  //   test2(); // true
-  //   test3(); // true
-  //   test4(); // false
-  //   test5(); // false
-  //   test6(); // true
+  test2(); // true
+  test3(); // false
+  test4(); // false
+  test5(); // false
+  test6(); // true
 }
---
+++ IV.programming-concepts/6.logic.js
@@ -0,0 +1,5 @@
+const flag1 = true;
+const flag2 = false;
+const flag3 = false;
+const result = flag1 || flag2 || flag3;
+console.log(result);
