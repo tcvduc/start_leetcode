ad0b04562c50cbeb42ba8501fbadde3f4e8dc879
tcvduc <caovanducs@gmail.com>
2023-05-12 19:31:07 +0700

    onboard algorithm

---
+++ again/4/20.js
@@ -0,0 +1,261 @@
+const Bracket = {
+  type: 1,
+  isOpen: true,
+  shape: "(",
+};
+
+/**
+ *
+ * @returns {Map<keyof,Bracket>} map
+ */
+function getBracketInfo() {
+  const map = new Map();
+
+  map.set("(", {
+    type: 1,
+    isOpen: true,
+    shape: "(",
+  });
+
+  map.set("[", {
+    type: 2,
+    isOpen: true,
+    shape: "[",
+  });
+
+  map.set("{", {
+    type: 3,
+    isOpen: true,
+    shape: "{",
+  });
+
+  map.set(")", {
+    type: 1,
+    isOpen: false,
+    shape: ")",
+  });
+
+  map.set("]", {
+    type: 2,
+    isOpen: false,
+    shape: "]",
+  });
+
+  map.set("}", {
+    type: 3,
+    isOpen: false,
+    shape: "}",
+  });
+
+  return map;
+}
+
+/**
+ *
+ * @param {String} s
+ */
+function stringToArray(s) {
+  const result = [];
+
+  for (let i = 0; i <= s.length - 1; ++i) result.push(s[i]);
+
+  return result;
+}
+
+/**
+ *
+ * @param {Array} a
+ * @param {Number} pos
+ *
+ */
+function array1dRemoveElementAtPos(a, pos) {
+  const result = new Array(a.length - 1);
+
+  for (let i = 0; i <= pos - 1; ++i) result[i] = a[i];
+
+  for (let j = pos; j <= result.length - 1; ++j) result[j] = a[j + 1];
+
+  return result;
+}
+
+/**
+ * @param {string} ss
+ * @return {boolean}
+ */
+var isValid = function (ss) {
+  let s = stringToArray(ss);
+  let flag = true;
+  let isAllOpenBracket = true;
+  const bracketInfo = getBracketInfo();
+
+  for (let i = 0; i <= s.length - 1; ) {
+    if (i < 0 && s.length !== 0) {
+      i++;
+    }
+
+    if (i < 0 && s.length === 0) {
+      break;
+    }
+
+    const si = bracketInfo.get(s[i]);
+
+    if (si.isOpen) {
+      i++;
+      continue;
+    }
+
+    if (!si.isOpen) {
+      isAllOpenBracket = false;
+
+      if (i - 1 < 0) {
+        flag = false;
+        break;
+      }
+
+      const sim1 = bracketInfo.get(s[i - 1]);
+
+      if (sim1.isOpen) {
+        if (sim1.type === si.type) {
+          const pos1 = i;
+          const pos2 = i - 1;
+          s = array1dRemoveElementAtPos(s, pos1);
+          s = array1dRemoveElementAtPos(s, pos2);
+          i -= 2;
+
+          continue;
+        }
+
+        if (sim1.type !== si.type) {
+          flag = false;
+          break;
+        }
+      }
+
+      if (!sim1.isOpen) {
+        flag = false;
+        break;
+      }
+    }
+  }
+
+  if (isAllOpenBracket || s.length !== 0) {
+    flag = false;
+  }
+
+  return flag;
+};
+
+function test1() {
+  const s = "()";
+  const result = isValid(s);
+  console.log(result);
+}
+
+function test2() {
+  const s = "()[]{}";
+  const result = isValid(s);
+  console.log(result);
+}
+
+function test3() {
+  const s = "(]";
+  const result = isValid(s);
+  console.log(result);
+}
+
+function test4() {
+  const s = "()[}[]";
+  const result = isValid(s);
+  console.log(result);
+}
+
+function test5() {
+  const s = "(][){}";
+  const result = isValid(s);
+  console.log(result);
+}
+
+function test6() {
+  const s = "()[]";
+  const result = isValid(s);
+  console.log(result);
+}
+
+function test7() {
+  const s = "{[]}";
+  const result = isValid(s);
+  console.log(result);
+}
+
+function test8() {
+  const s = "(([]){})";
+  const result = isValid(s);
+  console.log(result);
+}
+
+function test9() {
+  const s = "[";
+  const result = isValid(s);
+  console.log(result);
+}
+
+function test10() {
+  const s = "]";
+  const result = isValid(s);
+  console.log(result);
+}
+
+function test11() {
+  const s = "]]]";
+  const result = isValid(s);
+  console.log(result);
+}
+
+function test12() {
+  const s = "([]){";
+  const result = isValid(s);
+  console.log(result);
+}
+
+function test13() {
+  const s = "((";
+  const result = isValid(s);
+  console.log(result);
+}
+
+function test14() {
+  const s = "[[[]";
+  const result = isValid(s);
+  console.log(result);
+}
+
+function test15() {
+  const s = "()";
+  const result = isValid(s);
+  console.log(result);
+}
+
+function test16() {
+  const s = "{[]}";
+  const result = isValid(s);
+  console.log(result);
+}
+
+{
+  // test1(); // true
+  // test2(); // true
+  // test3(); // false
+  // test4(); // false
+  // test5(); // false
+  // test6(); // true
+  // test7(); // true
+  // test8(); // true
+  // test9(); // false
+  // test10(); // false
+  // test11(); // false
+  // test12(); // false
+  // test13(); // false
+  // test14(); // false
+  //   test15(); // false
+  test16();
+}
